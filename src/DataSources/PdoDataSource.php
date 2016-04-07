58d0767f461661ec1356dc03e9e4bd448ab9d1aa
Milan Felix Sulc <rkfelix@gmail.com>
2016-04-07 15:49:12 +0200

    DataSources: added new PdoDataSource

--- src/DataSources/PdoDataSource.php
+++ src/DataSources/PdoDataSource.php
@@ -2,11 +2,13 @@
 
 namespace Tlapnet\Report\DataSources;
 
-use Tlapnet\Report\Report\Report;
-use Tlapnet\Report\ReportBox\ParameterList;
+use PDO;
+use Tlapnet\Report\Model\Box\ParameterList;
+use Tlapnet\Report\Model\Data\Report;
 
 class PdoDataSource extends AbstractDatabaseDataSource
 {
+
 
 	/**
 	 * @param ParameterList $parameters
@@ -14,7 +16,23 @@ class PdoDataSource extends AbstractDatabaseDataSource
 	 */
 	public function compile(ParameterList $parameters)
 	{
-		$stop();
+		$pdo = new PDO(sprintf(
+			'%s:host=%s;dbname=%s',
+			$this->getConfig('driver'),
+			$this->getConfig('host'),
+			$this->getConfig('database')
+		),
+			$this->getConfig('user'),
+			$this->getConfig('password'));
+
+		$sql = $this->getSql();
+
+		$statement = $pdo->prepare($sql);
+		$statement->execute();
+
+		$report = new Report($statement->fetchAll());
+
+		return $report;
 	}
 
 }
