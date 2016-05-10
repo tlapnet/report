a4d001278384e4e3e25fff6c4a5ef1135f58e98f
Milan Felix Sulc <rkfelix@gmail.com>
2016-05-10 23:15:37 +0200

    Tests: fixes

--- tests/Report/Utils/DateTimeTest.php
+++ tests/Report/Utils/DateTimeTest.php
@@ -10,8 +10,8 @@ final class DateTimeTest extends BaseTestCase
 
 	public function testFrom()
 	{
-		$this->assertEquals('1.1.2000', DateTime::from('1.1.2000')->format('d.m.Y'));
-		$this->assertEquals('1.1.2000', DateTime::from(strtotime('1.1.2000'))->format('d.m.Y'));
+		$this->assertEquals('01.01.2000', DateTime::from('1.1.2000')->format('d.m.Y'));
+		$this->assertEquals('01.01.2000', DateTime::from(strtotime('1.1.2000'))->format('d.m.Y'));
 	}
 
 }
