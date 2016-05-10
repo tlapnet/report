e0f1d76034c3fffd1e3313befa6061b2a1616a27
Milan Felix Sulc <rkfelix@gmail.com>
2016-05-10 16:57:48 +0200

    Helpers: added toArray method

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -2,7 +2,35 @@
 
 namespace Tlapnet\Report\Utils;
 
-class Helpers
+use Tlapnet\Report\Model\Data\Result;
+
+final class Helpers
 {
+
+	/**
+	 * @param Result $result
+	 * @return array
+	 */
+	public static function toArray(Result $result)
+	{
+		// Fetch data
+		$data = $result->getData();
+		$array = [];
+
+		// Iterate over all rows
+		foreach ($data as $rn => $row) {
+			$arow = [];
+
+			// Iterate over all columns in a single row
+			foreach ((array)$row as $column => $cdata) {
+				$arow[$column] = (string)$cdata;
+			}
+
+			// Append row
+			$array[] = $arow;
+		}
+
+		return $array;
+	}
 
 }
