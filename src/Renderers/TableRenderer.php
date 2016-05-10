b206bd71d0642338e634d62ff3d815a9d9639d1c
Milan Felix Sulc <rkfelix@gmail.com>
2016-05-10 23:09:06 +0200

    Renderers: added new TableRenderer

--- src/Renderers/TableRenderer.php
+++ src/Renderers/TableRenderer.php
@@ -2,8 +2,8 @@
 
 namespace Tlapnet\Report\Renderers;
 
-use Tlapnet\Report\Exceptions\Logic\NotImplementedException;
 use Tlapnet\Report\Model\Data\Result;
+use Tlapnet\Report\Utils\Html;
 
 class TableRenderer implements Renderer
 {
@@ -14,7 +14,48 @@ class TableRenderer implements Renderer
 	 */
 	public function render(Result $report)
 	{
-		throw new NotImplementedException();
+		// Fetch data
+		$data = $report->getData();
+
+		// Create root element
+		$table = Html::el('table')
+			->addAttributes(['class' => 'table']);
+
+		// Create header
+		$header = Html::el('tr');
+		foreach ($data[0] as $column => $v) {
+			// Create th element
+			$th = Html::el('th')
+				->setHtml($column);
+
+			// Append to header
+			$header->add($th);
+		}
+		$table->add($header);
+
+
+		// Iterate over all rows
+		foreach ($data as $row) {
+
+			// Creat tr element
+			$tr = Html::el('tr');
+
+			// Iterate over all columns
+			foreach ($row as $column => $cdata) {
+
+				// Create td element
+				$td = Html::el('td');
+				$td->setHtml((string)$cdata);
+
+				// Append to tr
+				$tr->add($td);
+			}
+
+			// Add tds to tr
+			$table->add($tr);
+		}
+
+		return $table;
 	}
 
 }
