39eb81e3b4892af252a63f99159844bab21f6a62
Milan Felix Sulc <rkfelix@gmail.com>
2016-05-17 10:32:12 +0200

    Tests: added test for EditableSubreport

--- src/Model/Subreport/EditableSubreport.php
+++ src/Model/Subreport/EditableSubreport.php
@@ -58,6 +58,7 @@ class EditableSubreport extends Subreport
 	public function setResult(Resultable $result)
 	{
 		$this->result = $result;
+		$this->rawResult = $result;
 	}
 
 }
---
+++ tests/Report/Model/Subreport/EditableSubreportTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tlapnet\Report\Tests\Model\Subreport;
+
+use Tlapnet\Report\DataSources\DevNullDataSource;
+use Tlapnet\Report\Model\Data\Result;
+use Tlapnet\Report\Model\Preprocessor\Preprocessors;
+use Tlapnet\Report\Model\Subreport\EditableSubreport;
+use Tlapnet\Report\Model\Subreport\Parameters;
+use Tlapnet\Report\Model\Utils\Metadata;
+use Tlapnet\Report\Renderers\DevNullRenderer;
+use Tlapnet\Report\Tests\BaseTestCase;
+
+final class EditableSubreportTest extends BaseTestCase
+{
+
+	public function testDefault()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+
+		$this->assertSame(Parameters::class, get_class($er->getParameters()));
+		$this->assertSame(DevNullDataSource::class, get_class($er->getDataSource()));
+		$this->assertSame(DevNullRenderer::class, get_class($er->getRenderer()));
+		$this->assertNotNull($er->getMetadata());
+		$this->assertNull($er->getResult());
+		$this->assertNull($er->getRawResult());
+	}
+
+	public function testSetParameters()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setParameters($p = new Parameters([]));
+
+		$this->assertSame($p, $er->getParameters());
+	}
+
+	public function testSetDataSource()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setDataSource($d = new DevNullDataSource());
+
+		$this->assertSame($d, $er->getDataSource());
+	}
+
+	public function testSetRenderer()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setRenderer($r = new DevNullRenderer());
+
+		$this->assertSame($r, $er->getRenderer());
+	}
+
+	public function testSetResult()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setResult($r = new Result([]));
+
+		$this->assertSame($r, $er->getResult());
+		$this->assertSame($r, $er->getRawResult());
+	}
+
+	public function testSetMetadata()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setMetadata($m = new Metadata([]));
+
+		$this->assertSame($m, $er->getMetadata());
+	}
+
+	public function testSetPreprocessors()
+	{
+		$er = new EditableSubreport('s1', new Parameters([]), new DevNullDataSource(), new DevNullRenderer());
+		$er->setPreprocessors($p = new Preprocessors());
+
+		$this->assertSame($p, $er->getPreprocessors());
+	}
+
+}
