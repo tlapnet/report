1d124ef93297a2d752a3f0853390ac9643c3d71d
Milan Felix Šulc <rkfelix@gmail.com>
2017-04-21 11:03:06 +0200

    Feature: specific preprocessors for each key/value in VerticalTable [closes #29]

--- src/Preprocessor/Preprocessors.php
+++ src/Preprocessor/Preprocessors.php
@@ -48,6 +48,7 @@ class Preprocessors
 		// Fetch data
 		$data = $result->getData();
 		$newdata = [];
+		$pointer = 0;
 
 		// Iterate over all rows
 		foreach ($data as $key => $row) {
@@ -55,19 +56,34 @@ class Preprocessors
 			// If row is scalar value, we're assuming it as key-value dataset.
 			// Otherwise, it's classic array/structure dataset.
 			if (is_scalar($row)) {
-				// Get preprocessors for key
-				$preprocessors1 = isset($this->preprocessors[self::DATASET_KEY]) ? $this->preprocessors[self::DATASET_KEY] : [];
-				foreach ($preprocessors1 as $p1) {
+				// Get preprocessors for generic key
+				$preprocessors1a = isset($this->preprocessors[self::DATASET_KEY]) ? $this->preprocessors[self::DATASET_KEY] : [];
+				foreach ($preprocessors1a as $p1) {
 					// chain-preprocessing...
 					$key = $p1->preprocess($key);
 				}
 
-				// Get preprocessors for value
-				$preprocessors2 = isset($this->preprocessors[self::DATASET_VALUE]) ? $this->preprocessors[self::DATASET_VALUE] : [];
-				foreach ($preprocessors2 as $p2) {
+				// Get preprocessors for specific key0-N
+				$preprocessors1b = isset($this->preprocessors[self::DATASET_KEY . $pointer]) ? $this->preprocessors[self::DATASET_KEY . $pointer] : [];
+				foreach ($preprocessors1b as $p1) {
+					// chain-preprocessing...
+					$key = $p1->preprocess($key);
+				}
+
+				// Get preprocessors for generic value
+				$preprocessors2a = isset($this->preprocessors[self::DATASET_VALUE]) ? $this->preprocessors[self::DATASET_VALUE] : [];
+				foreach ($preprocessors2a as $p2) {
 					// chain-preprocessing...
 					$row = $p2->preprocess($row);
 				}
+
+				// Get preprocessors for specific value0-N
+				$preprocessors2b = isset($this->preprocessors[self::DATASET_VALUE . $pointer]) ? $this->preprocessors[self::DATASET_VALUE . $pointer] : [];
+				foreach ($preprocessors2b as $p2) {
+					// chain-preprocessing...
+					$row = $p2->preprocess($row);
+				}
+
 			} else {
 				// Iterate over all columns in a single row
 				foreach ((array) $row as $column => $columnData) {
@@ -86,6 +102,9 @@ class Preprocessors
 
 			// Update row data
 			$newdata[$key] = $row;
+
+			// Update pointer
+			$pointer++;
 		}
 
 		// Set a preprocessed data
