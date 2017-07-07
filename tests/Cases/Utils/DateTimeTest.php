220b269cadc4c2b2c69c5975d2697819561e5c33
Milan Felix Šulc <rkfelix@gmail.com>
2017-07-07 17:15:21 +0200

    Feature: enhance test for DateTime

--- tests/Cases/Utils/DateTimeTest.php
+++ tests/Cases/Utils/DateTimeTest.php
@@ -19,4 +19,19 @@ final class DateTimeTest extends BaseTestCase
 		$this->assertEquals('01.01.2000', DateTime::from(strtotime('1.1.2000'))->format('d.m.Y'));
 	}
 
+	/**
+	 * @covers DateTime::format
+	 * @return void
+	 */
+	public function testFormat()
+	{
+		$dt = DateTime::from('20.01.2000 10:11:12');
+		$this->assertEquals('Y-m-d H:i:s', $dt->getFormat());
+		$this->assertEquals('2000-01-20 10:11:12', $dt->__toString());
+
+		$dt->setFormat('Y-m-d');
+		$this->assertEquals('Y-m-d', $dt->getFormat());
+		$this->assertEquals('2000-01-20', $dt->__toString());
+	}
+
 }
